9225a4c05142fae00a92f7a54ca734dab1016a48
phoebe skuncc <70713395+skuncc@users.noreply.github.com>
2024-04-13 15:36:24 -0400

    megahaul update.
    game works :3 its boring, tho. going to keep using these textures until we get a working dialogue and inventory system going.
    
    aaaand lots of other stuff, too >..>;;

--- if you summon, fae will come/scripts/player.gd
+++ if you summon, fae will come/scripts/player.gd
@@ -1,28 +1,27 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const speed = 100
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	player_movement(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = -speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		velocity.y = 0
+	
 	move_and_slide()
+	
