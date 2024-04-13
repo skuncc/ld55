5f12693827d022b42d300e782de81fd8e9da7af1
phoebe skuncc <70713395+skuncc@users.noreply.github.com>
2024-04-13 19:16:12 -0400

    lil update

--- if you summon, fae will come/scripts/player.gd
+++ if you summon, fae will come/scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const speed = 155
+var current_dir = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -8,15 +9,19 @@ func _physics_process(delta):
 func player_movement(delta):
 	
 	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
 		velocity.x = 0
 		velocity.y = -speed
 	else:
@@ -24,4 +29,20 @@ func player_movement(delta):
 		velocity.y = 0
 	
 	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_jogging")
+		elif movement == 0:
+			anim.play("side_idle")
+			
+	
+	
+	
+	
 	
