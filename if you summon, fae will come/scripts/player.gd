4482b46a4a3cb3c1b1eeb1fdcd1a14b7af614269
phoebe skuncc <70713395+skuncc@users.noreply.github.com>
2024-04-13 23:32:24 -0400

    awa 1

--- if you summon, fae will come/scripts/player.gd
+++ if you summon, fae will come/scripts/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 const speed = 155
 var current_dir = "none"
 
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+
 func _physics_process(delta):
 	player_movement(delta)
 
@@ -10,21 +14,26 @@ func player_movement(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = -speed
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 	
@@ -40,9 +49,25 @@ func play_anim(movement):
 			anim.play("side_jogging")
 		elif movement == 0:
 			anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_jogging")
+		elif movement == 0:
+			anim.play("side_idle")
 			
-	
-	
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_jogging")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("back_jogging")
+		elif movement == 0:
+			anim.play("back_idle")
 	
 	
 	
